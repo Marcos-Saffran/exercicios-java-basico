6c4ff863650448f98b201cf0e69e38e547341c7a
Marcos Alfredo Saffran <marcossaffran@gmail.com>
2026-06-02 11:36:06 -0300

    feat: implement pet washing machine functionality, in exercise 3

---
+++ exercicios/exercicio-3-diversos/petshop-bath_control/src/Pet.java
@@ -0,0 +1,25 @@
+public class Pet {
+
+    private final String name;
+
+    private boolean clean;
+
+    public Pet(String name) {
+        this.name = name;
+        this.clean = false;
+    }
+
+    public void setClean(boolean clean) {
+        this.clean = clean;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isClean() {
+        return clean;
+    }
+
+
+}
